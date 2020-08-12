75003d42807c1e6e845a3c39c5eeb4cb8f18a4a4
Hunter Danielson <hunterleedanielson@gmail.com>
2020-08-12 00:23:27 -0700

    added user signup/login

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
 export const fetchRecipes = () => {
-  return fetch('http://localhost:7890/api/v1/recipes')
+  return fetch(`${process.env.API_URL}/api/v1/recipes`)
     .then(res => res.json());
 };
